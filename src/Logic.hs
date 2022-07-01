649b8f73816c123ecb886510a1a3080d48a3bdf6
Baitinq <manuelpalenzuelamerino@gmail.com>
2022-07-01 20:30:26 +0200

    Implemented predictSkyObjects

--- src/Logic.hs
+++ src/Logic.hs
@@ -3,30 +3,47 @@ module Logic where
 import Data.Astro.Types
 import Data.Astro.Time.JulianDate
 import Data.Astro.Coordinate
+import Data.Astro.Planet
 
-data SkyT = Sky { date :: JulianDate,
+data SkyT = Sky { s_date :: JulianDate,
+                  objects :: [SkyObjectT]
+                  } deriving (Show)
+
+data RelativeSkyT = RelativeSky { rs_date :: JulianDate,
                   nonVisibleObjects :: [SkyObjectT],
                   visibleObjects :: [SkyObjectT]
                   } deriving (Show)
 
 data SkyObjectT = SkyObject {
-    name :: String
-    --coordinates :: EquatorialCoordinates1
-    --m_relative_coordinates :: Maybe HorizonCoordinates
+    so_name :: String,
+    coordinates :: EquatorialCoordinates1
+} deriving (Show)
+
+data RelativeSkyObjectT = RelativeSkyObject {
+    rso_name :: String
+    --m_relative_coordinates :: HorizonCoordinates
 } deriving (Show)
 
 generateSky :: JulianDate -> SkyT
-generateSky date = let
-                                (nonVisibleObjects, visibleObjects) = predictSkyObjects date
-                            in 
-                                Sky { 
-                                    date = date,
-                                    nonVisibleObjects=nonVisibleObjects, 
-                                    visibleObjects=visibleObjects
-                                 }
+generateSky date = Sky { 
+                            s_date = date,
+                            objects = predictSkyObjects date
+                        }
 
-predictSkyObjects :: JulianDate -> ([SkyObjectT], [SkyObjectT])
-predictSkyObjects date = ([SkyObject { name="test" }], [SkyObject { name= "uwu" }])
+trackedObjects :: [Planet]
+trackedObjects = [Mercury, Venus, Mars, Jupiter, Saturn, Neptune, Uranus] --Moon?
 
-relativize_sky :: SkyT -> GeographicCoordinates -> SkyT
-relativize_sky sky location = sky
+predictSkyObjects :: JulianDate -> [SkyObjectT]
+predictSkyObjects date = do
+                            object <- trackedObjects
+                            objectDetails <- return $ j2010PlanetDetails object
+                            earthDetails <- return $ j2010PlanetDetails Earth
+                            objectPosition <- return $ planetPosition planetTrueAnomaly1 objectDetails earthDetails date
+                            return SkyObject { so_name = show object, coordinates = objectPosition }
+
+relativize_sky :: SkyT -> GeographicCoordinates -> RelativeSkyT
+relativize_sky sky location = RelativeSky {
+                                             rs_date = s_date sky,
+                                             nonVisibleObjects = objects sky,
+                                             visibleObjects = []
+                                          }
